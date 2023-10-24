4b542b4363ad4c77e687a184a51033bcf1c0bf1a
Basic HTML and Javascripts <132616327+AnneShanthakumar@users.noreply.github.com>
2023-10-23 19:54:57 -0700

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/MathOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class MathOperations
+{
+    // Void method that takes two integers as parameters, performs a math operation, and displays the second integer
+    public void PerformMathOperation(int number1, int number2)
+    {
+        // For demonstration, this method adds the two numbers
+        int result = number1 + number2;
+        Console.WriteLine($"Result: {result}");
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/MethodClassAssignment.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        // Instantiate the MathOperations class
+        MathOperations mathOperations = new MathOperations();
+
+        // Call the method in the class, passing in two numbers
+        Console.WriteLine("Calling method with two numbers:");
+        mathOperations.PerformMathOperation(5, 7);
+
+        // Call the method in the class, specifying the parameters by name
+        Console.WriteLine("\nCalling method with named parameters:");
+        mathOperations.PerformMathOperation(number1: 10, number2: 3);
+    }
+}
