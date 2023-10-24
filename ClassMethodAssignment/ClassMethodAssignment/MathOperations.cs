3579c2dbff0eb2f7465f8c873acbff02b1ec0939
Basic HTML and Javascripts <132616327+AnneShanthakumar@users.noreply.github.com>
2023-10-23 20:12:05 -0700

    Add files via upload

---
+++ ClassMethodAssignment/ClassMethodAssignment/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class MathOperations
+{
+    // Void method that outputs an integer (dividing the data passed by 2)
+    public void DivideByTwo(int number)
+    {
+        int result = number / 2;
+        Console.WriteLine($"Result: {result}");
+    }
+
+    // Method with output parameters
+    public void MultiplyByTwo(int input, out int result)
+    {
+        result = input * 2;
+    }
+
+    // Overloaded method
+    public void MultiplyByTwo(double input)
+    {
+        double result = input * 2;
+        Console.WriteLine($"Result (overloaded method): {result}");
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        // Instantiate the MathOperations class
+        MathOperations mathOperations = new MathOperations();
+
+        // Have the user enter a number
+        Console.Write("Enter a number: ");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+
+        // Call the void method on the entered number
+        mathOperations.DivideByTwo(userInput);
+
+        // Call the method with output parameters
+        int outputResult;
+        mathOperations.MultiplyByTwo(10, out outputResult);
+        Console.WriteLine($"Result (output parameter method): {outputResult}");
+
+        // Call the overloaded method
+        mathOperations.MultiplyByTwo(3.5);
+    }
+}
