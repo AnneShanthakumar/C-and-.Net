44a183dbd9fdaf429fd2c2d7e842f5460de65427
Basic HTML and Javascripts <132616327+AnneShanthakumar@users.noreply.github.com>
2023-10-23 19:59:50 -0700

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp4/ConsoleApp4/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿public class MathOperations
+{
+    // Method to perform math operation on an integer
+    public int DoMathOperation(int number)
+    {
+        // For demonstration, simply square the input
+        return number * number;
+    }
+
+    // Method to perform a different math operation on a decimal
+    public int DoMathOperation(decimal number)
+    {
+        // For demonstration, double the input
+        return (int)(number * 2);
+    }
+
+    // Method to perform a different math operation on a string (converted to an integer)
+    public int DoMathOperation(string input)
+    {
+        // For demonstration, add 10 to the integer value of the string
+        if (int.TryParse(input, out int number))
+        {
+            return number + 10;
+        }
+        else
+        {
+            // Return -1 to indicate that the string couldn't be converted to an integer
+            return -1;
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOperations = new MathOperations();
+
+            // Call the first method with an integer
+            int result1 = mathOperations.DoMathOperation(5);
+            Console.WriteLine("Result 1: " + result1);
+
+            // Call the second method with a decimal
+            int result2 = mathOperations.DoMathOperation(3.5m);
+            Console.WriteLine("Result 2: " + result2);
+
+            // Call the third method with a string (which can be converted to an integer)
+            int result3 = mathOperations.DoMathOperation("7");
+            Console.WriteLine("Result 3: " + result3);
+        }
+    }
+}
