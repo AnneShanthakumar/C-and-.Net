a3f845fd8c7f90e1041bcacb536d3543b39d4e8d
Basic HTML and Javascripts <132616327+AnneShanthakumar@users.noreply.github.com>
2023-10-24 12:32:49 -0700

    Add files via upload

---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Prompt user for input
+            Console.Write("Please enter the current day of the week: ");
+
+            // Read user input and convert it to the enum type
+            DayOfWeek userInput = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine(), true);
+
+            // Print the selected day
+            Console.WriteLine($"You entered: {userInput}");
+        }
+        catch (Exception)
+        {
+            // Print error message if an exception occurs
+            Console.WriteLine("Please enter an actual day of the week.");
+        }
+    }
+}
