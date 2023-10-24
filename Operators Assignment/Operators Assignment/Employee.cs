062685733817db14d7371c6937e18a523cc96c01
Basic HTML and Javascripts <132616327+AnneShanthakumar@users.noreply.github.com>
2023-10-24 12:04:23 -0700

    Add files via upload

---
+++ Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿public class Employee
+{
+    // Properties
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Overload the "==" operator to compare Employees by Id
+    public static bool operator ==(Employee emp1, Employee emp2)
+    {
+        return emp1.Id == emp2.Id;
+    }
+
+    // Overload the "!=" operator for consistency
+    public static bool operator !=(Employee emp1, Employee emp2)
+    {
+        return !(emp1 == emp2);
+    }
+}
---
+++ Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Create two Employee objects
+        Employee emp1 = new Employee
+        {
+            Id = 1,
+            FirstName = "Dave",
+            LastName = "Doe"
+        };
+
+        Employee emp2 = new Employee
+        {
+            Id = 2,
+            FirstName = "Jane",
+            LastName = "Doe"
+        };
+
+        // Compare employees using overloaded operators
+        bool areEqual = (emp1 == emp2);
+
+        // Display the result
+        Console.WriteLine($"Employees are equal: {areEqual}");
+    }
+}
